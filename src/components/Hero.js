6d74b5b16b1e52ffb48c34039c36c8ec8f25146e
Kingstalux <kingsleyngu25@gmail.com>
2022-07-05 04:55:41 +0100

    Finish homepage

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,21 +3,31 @@ import WomanImg from '../assets/img/banner-woman2.webp'
 
 export const Hero = () => {
     return (
-        <section id="home" className="lg:h-{85vh} flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden">
-
-            <div className="container mx-auto h-full">
-
-                <div className="flex items-center h-full pt-8">
-
-                    <div className="flex-1 flex">left</div>
-
-                    <div className="hidden lg:flex flex-1 justify-end items-end h-full">
-                        <img src={WomanImg} alt="my_picture" />
-                    </div>
-
-                </div>
-            </div>
-
-        </section>
+    <section
+      id='home'
+      className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'
+    >
+      <div className='container mx-auto h-full'>
+        <div className='flex items-center h-full pt-8'>
+          <div className='flex-1 flex flex-col items-center lg:items-start'>
+            <p className='text-lg text-accent text-md mb-[22px]'>
+              Hey there, I'm Kingsely! 👋
+            </p>
+            <h1 className='text-4xl leading-[44px] md:text-5xl md:leading-tight lg:text-7xl lg:leading-[1.2] font-bold md:tracking-[-2px]'>
+              I Build & Design <br /> Web Interfaces.
+            </h1>
+            <p className='pt-4 pb-8 md:pt-6 md:pb-12 max-w-[480px] text-lg text-center lg:text-left'>
+              I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don't hesitate to contact me.
+            </p>
+            <button className='btn btn-md bg-accent hover:bg-secondary-hover md:btn-lg transition-all'>
+              Let's Connect
+            </button>
+          </div>
+          <div className='hidden lg:flex flex-1 justify-end items-end h-full'>
+            <img src={WomanImg} alt='' />
+          </div>
+        </div>
+      </div>
+    </section>
     )
 }
