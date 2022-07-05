09ccf17b3b2fd48ab2ce613910f557dbd44d0a89
Kingstalux <kingsleyngu25@gmail.com>
2022-07-05 05:15:41 +0100

    Add projects

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { About } from './components/About';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
+import { Portfolio } from './components/Portfolio';
 import { Skills } from './components/Skills';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Skills />
+      <Portfolio />
       <div style={{height: '2000px'}}></div>
     </div>
   );
