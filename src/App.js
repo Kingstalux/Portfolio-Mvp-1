8e0ba74fe0e86396698e5c5325d041f819903ae3
Kingstalux <kingsleyngu25@gmail.com>
2022-07-05 05:04:58 +0100

    Add about page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { About } from './components/About';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 
@@ -7,6 +8,7 @@ const App = () => {
     <div>
       <Header />
       <Hero />
+      <About />
       <div style={{height: '2000px'}}></div>
     </div>
   );
