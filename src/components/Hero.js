7cb85bfcbf0bd5d8d5cc1d2a8281059e8eba197f
Kingstalux <kingsleyngu25@gmail.com>
2022-07-05 04:35:27 +0100

    Add home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import WomanImg from '../assets/img/banner-woman2.webp'
 
 export const Hero = () => {
-  return (
-    <section className="lg:h-{85vh} flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden">
-        <div className="container mx-auto">hero</div>
-    </section>
-  )
+    return (
+        <section id="home" className="lg:h-{85vh} flex items-center bg-primary lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden">
+
+            <div className="container mx-auto h-full">
+
+                <div className="flex items-center h-full pt-8">
+
+                    <div className="flex-1 flex">left</div>
+
+                    <div className="hidden lg:flex flex-1 justify-end items-end h-full">
+                        <img src={WomanImg} alt="my_picture" />
+                    </div>
+
+                </div>
+            </div>
+
+        </section>
+    )
 }
