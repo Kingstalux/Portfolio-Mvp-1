2988caa6f6679b9cc01fd289c0fc948602e26072
Kingstalux <kingsleyngu25@gmail.com>
2022-07-05 05:52:29 +0100

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { About } from './components/About';
 import { Contacts } from './components/Contacts';
+import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { Portfolio } from './components/Portfolio';
@@ -19,6 +20,7 @@ const App = () => {
       <Services />
       <Testimonials />
       <Contacts />
+      <Footer />
       <div style={{height: '2000px'}}></div>
     </div>
   );
